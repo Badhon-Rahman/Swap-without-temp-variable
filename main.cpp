1a21556e884162f5a795eded924cfef83e9e8992
BADHON RAHMAN <badhon.mostafizur@gmail.com>
2020-01-07 18:32:44 +0600

    swap two element

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    double firstNumber, secondNumber;
+    printf("Enter first number: ");
+    scanf("%lf", &firstNumber);
+    printf("Enter second number: ");
+    scanf("%lf",&secondNumber);
+
+    firstNumber = firstNumber - secondNumber;
+    secondNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber - firstNumber;
+    printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+    printf("After swapping, secondNumber = %.2lf", secondNumber);
+    return 0;
+}
